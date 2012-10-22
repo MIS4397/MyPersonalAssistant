d0995ec8f2fb47010159cfcee553babff1d1bbca
Khalid Adil <khalidadil29@gmail.com>
2012-10-22 04:20:02 -0500

    8th Commit

--- php/createAccount.php
+++ php/createAccount.php
@@ -1,41 +1,16 @@
 <?php
-/* Define some constants
-define( "RECIPIENT_NAME", "John Smith" );
-define( "RECIPIENT_EMAIL", "john@example.com" );
-define( "EMAIL_SUBJECT", "Visitor Message" );*/
-// Read the form values
+//Define variables
 $createaccnt = $_POST['createaccnt'];
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $loginEmail = $_POST['loginEmail'];
-$password = isset( $_POST['password'] ) ? preg_replace( "/[^\.\-\' a-
+/*$password = isset( $_POST['password'] ) ? preg_replace( "/[^\.\-\' a-
 zA-Z0-9]/", "", $_POST['password'] ) : "";
 $confirmpass = isset( $_POST['confirmpass'] ) ? preg_replace( "/[^\.\-\' a-
-zA-Z0-9]/", "", $_POST['confirmpass'] ) : "";
-
-/*$success = false;
-$senderName = isset( $_POST['senderName'] ) ? preg_replace( "/[^\.\-\' a-
-zA-Z0-9]/", "", $_POST['senderName'] ) : "";
-$senderEmail = isset( $_POST['senderEmail'] ) ? preg_replace( "/[^\.\-\_
-\@a-zA-Z0-9]/", "", $_POST['senderEmail'] ) : "";
-$productCategories = array();
-if ( isset( $_POST['productCategories'] ) ) {
-  foreach ( $_POST['productCategories'] as $cat ) $productCategories[] =
-preg_replace( "/[^\'\-\ a-zA-Z0-9]/", "", $cat );
-}
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|
-CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-if ( $productCategories ) {
-  $message .= "\n\n---\n\nInterested in product categories:\n\n";
-  foreach ( $productCategories as $cat ) $message .= "$cat\n";
-￼￼}
-// If all values exist, send the email
-if ( $senderName && $senderEmail && $message ) {
-  $recipient = Khalid Adil . " <" . khalidadil29@gmail.com . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
-  $success = mail( $recipient, TEST_EMAIL, $message, $headers );
-}*/
+zA-Z0-9]/", "", $_POST['confirmpass'] ) : "";*/
 // Return an appropriate response to the browser
+echo $createacct;
+echo $firstName;
 ?>
 <!doctype html>
 <html>
@@ -45,37 +20,25 @@ if ( $senderName && $senderEmail && $message ) {
 </head>
 <body>
   <div data-role="page" id="contactResult">
-    <div data-role="header">
-      <h1>Hairy Hippo</h1>
+    
+<div data-role="header">
+      <h1>myPersonalAttendant</h1>
 </div>
-    <div data-role="content">
-<?php echo "/n".$createacct."/n".$firstName."/n".$lastName."/n".$loginEmail."/n".$password ."/n"?>
-<?php if ( isset( $_POST['firstName'] )  { ?>
-      <div style="text-align: center;">
-        <h2>Thanks!</h2>
-        <img src="images/juggling_mom.jpeg" width="200" alt="Logo">
-        <p>Thanks for sending your message! We'll get back to you shortly.</p>
-      </div>
-<?php } else { ?>
-      <div style="text-align: center;">
-        <h2>Oops!</h2>
-        <p style="color: red">
-There was a problem sending your message. Please make sure you
-fill in all the fields in the form.<br><br>
-          <a href="#contact" data-rel="back" data-role="button">Try Again</a>
-</p>
-      </div>
-<?php } ?>
+    
+<div data-role="content">
+<?php
+echo $createacct;
+echo $firstName;
+/*$con = mysql_connect("localhost","root","root");
+if (!$con)
+  {
+  	die('Could not connect: ' . mysql_error());
+  }
+mysql_select_db("appdb", $con);
+mysql_close($con)*/
+
+?>
 </div>
-   <xsdiv data-role="footer" data-position="fixed" data-id="nav">
-       <!--<div data-role="navbar">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#products">Products</a></li>
-          <li><a href="#contact" class="ui-btn-active ui-state-persist">Contact Us</a></li>
-        </ul>
-      </div>-->
-    </div>
-</div>
+	</div>
 </body>
 </html>
